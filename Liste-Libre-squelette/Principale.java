c9d1ada1ccc33fe99b69b93c0b8cda8cc12d765a
Geoffrey2501 <132345682+Geoffrey2501@users.noreply.github.com>
2024-01-17 20:29:21 +0100

    fini

--- Liste-Libre-squelette/Principale.java
+++ Liste-Libre-squelette/Principale.java
@@ -53,15 +53,17 @@ public class Principale{
         String[] liste_noms = lf.lireFichier();
         ListeTriee l;
         int nbPlace=10000;
+        int sum=0;
         if(c=='a' || type==1)nbPlace+=1000;
+        
+
+        for(int i=0; i<100; i++){
         if(type==1)l=new ListeTriee(new ListeContigue(nbPlace));
         else if(type==2)l=new ListeTriee(new ListeChainee(nbPlace));
         else l=new ListeTriee(new ListeChaineePlacesLibres(nbPlace));
-        int sum=0;
+        
 
         for(int r=0;r<10000;r++)l.adjlisT(liste_noms[r]);
-
-        for(int i=0; i<100; i++){
             long date_debut = System.nanoTime(); 
             for(int j=0; j<10;j++){
                 if (c=='a') {
@@ -77,7 +79,7 @@ public class Principale{
             long date_fin = System.nanoTime();
             sum+=date_fin-date_debut;
         }
-        return sum/1000;
+        return sum/100;
     }
 
 
@@ -90,34 +92,17 @@ public class Principale{
 	//fichier.ouvrirFichier();
 	//fichier.ecrireLigne("liste;operation;emplacement;duree");
 	
-/*
+
     ListeTriee lCh= new ListeTriee(new ListeChainee(10000));
     ListeTriee lChP= new ListeTriee(new ListeChaineePlacesLibres(10000));
     ListeTriee lCo= new ListeTriee(new ListeContigue(10000));
     
     //remplissage
-    System.out.println('1');
-    System.out.println(testChrono(1,'a', 'd'));
-    System.out.println(testChrono(2,'a', 'd'));
-    System.out.println(testChrono(3,'a', 'd'));
-    System.out.println('2');
-    System.out.println(testChrono(1,'a', 'f'));
-    System.out.println(testChrono(2,'a', 'f'));
-    System.out.println(testChrono(3,'a', 'f'));
-    System.out.println('3');
-    System.out.println(testChrono(1,'s', 'd'));
-    System.out.println(testChrono(2,'s', 'd'));
-    System.out.println(testChrono(3,'s', 'd'));
-    System.out.println('4');
-    
-    System.out.println(testChrono(1,'s', 'f'));
-    System.out.println(testChrono(2,'s', 'f'));
-    System.out.println(testChrono(3,'s', 'f'));
-*/
+
     //remplissage (vrai)
     EcritureFichier fichier = new EcritureFichier("resultats.csv");
     fichier.ouvrirFichier();
-    for(int i=0; i<4; i++){
+    for(int i=0; i<3; i++){
         for(int j=0; j<3; j++){
             String li="";
             String operation;
@@ -150,8 +135,7 @@ public class Principale{
 
         }
     }
-
-        //fichier.fermerFichier();
+    fichier.fermerFichier();
     }
 
 
